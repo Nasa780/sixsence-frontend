e2ecd65d0dd6cf525660cf7312fd168f6b429834
Onder <weispecher.onder@gmail.com>
2026-06-30 18:48:03 +0200

    Refonte de la page d'accueil : structure évolutive, alignement pixel-perfect, intégration des assets individuels

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,20 @@ export default function Home() {
       {/* TON FIGMA PARFAIT */}
       <img
         src="/assets/landing.png"
-        className="w-[2189px] h-[1304px]"
+        className="w-[2189px] h-[1304px] z-0"
         alt="Six Sence Landing"
       />
 
       {/* BOUTON DISCORD (HEADER) */}
       <div
-        className="absolute cursor-pointer"
+        className="absolute cursor-pointer z-10"
         style={{
           left: "1850px",
           top: "40px",
           width: "200px",
           height: "70px",
+          // bg: test visuel
+          // backgroundColor: "rgba(255,0,0,0.3)"
         }}
         onClick={() =>
           window.location.href = "https://sixsence-backend.onrender.com/auth/discord"
@@ -25,24 +27,26 @@ export default function Home() {
 
       {/* BOUTON JOUEZ GRATUITEMENT */}
       <div
-        className="absolute cursor-pointer"
+        className="absolute cursor-pointer z-10"
         style={{
           left: "730px",
           top: "520px",
           width: "300px",
           height: "70px",
+          // backgroundColor: "rgba(0,255,0,0.3)"
         }}
         onClick={() => window.location.href = "/accueil"}
       />
 
       {/* BOUTON DISCORD (BLEU) */}
       <div
-        className="absolute cursor-pointer"
+        className="absolute cursor-pointer z-10"
         style={{
           left: "730px",
           top: "600px",
           width: "300px",
           height: "70px",
+          // backgroundColor: "rgba(0,0,255,0.3)"
         }}
         onClick={() =>
           window.location.href = "https://sixsence-backend.onrender.com/auth/discord"
