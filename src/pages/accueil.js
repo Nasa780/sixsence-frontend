adeb6b2ce42a7cefddd803ca43a5f09d70b81664
Onder <weispecher.onder@gmail.com>
2026-07-01 08:08:56 +0200

    Refonte de la page d'accueil : structure évolutive, alignement pixel-perfect, intégration des assets individuel

--- src/pages/accueil.js
+++ src/pages/accueil.js
@@ -1,9 +1,11 @@
-import Navigationaccueil from "../components/navigationaccueil";
+import Navigationaccueil from "../components/Navigationaccueil";
+import Containeraccueil from "../components/Containeraccueil";
 
 export default function Accueil() {
   return (
     <div>
       <Navigationaccueil />
+      <Containeraccueil />  
       {/* reste de ta page */}
     </div>
   );
