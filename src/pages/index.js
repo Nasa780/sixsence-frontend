b88af97d35a8ea3b7b2e11705c8583dd70afdf3b
Onder <weispecher.onder@gmail.com>
2026-07-01 02:30:14 +0200

    Refonte de la page d'accueil : structure évolutive, alignement pixel-perfect, intégration des assets individuel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import MainContent from "../components/MainContent";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen bg-black text-white">
       <Header />
+      <MainContent />
     </div>
   );
 }
