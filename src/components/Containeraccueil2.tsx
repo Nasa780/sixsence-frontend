9c78c5242efb9cf1deac5fca45c44eb650145103
Onder <weispecher.onder@gmail.com>
2026-07-01 10:12:02 +0200

    Refonte de la page d'accueil : structure évolutive, alignement pixel-perfect, intégration des assets individuel

--- src/components/Containeraccueil2.tsx
+++ src/components/Containeraccueil2.tsx
@@ -1,14 +1,12 @@
-import type { NextPage } from "next";
 import Image from "next/image";
 
-const Containeraccueil2: NextPage = () => {
+const Containeraccueil2 = () => {
   return (
-    <div className="w-[2174px] max-w-full flex flex-col items-center pt-0 px-0 pb-6 box-border opacity-[0.3] leading-[normal] tracking-[normal]">
+    <div className="w-full max-w-[2174px] mx-auto flex flex-col items-center pt-0 px-0 pb-6 box-border opacity-100 leading-[normal] tracking-[normal]">
       <Image
-        className="w-full h-[18px] relative"
-        width={18}
-        height={18}
-        sizes="100vw"
+        className="w-full h-[40px] relative"
+        width={40}
+        height={40}
         alt=""
         src="/assets/Iconfleche2.svg"
       />
