ae1633759fa9dc280d7b3d22fd3970f98f6cb36e
Onder <weispecher.onder@gmail.com>
2026-07-01 03:43:26 +0200

    Refonte de la page d'accueil : structure évolutive, alignement pixel-perfect, intégration des assets individuel

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,14 +5,14 @@ const Container: NextPage = () => {
   return (
     <div className="w-[236px] shadow-[0px_12px_48px_rgba(0,_0,_0,_0.55),_0px_1px_0px_rgba(255,_255,_255,_0.05)_inset] rounded-[5px] bg-[rgba(9,9,16,0.88)] border-[rgba(255,255,255,0.07)] border-solid border-[1px] box-border flex flex-col items-start pt-[17px] px-[15px] pb-[15px] relative isolate leading-[normal] tracking-[normal] z-[0]">
 
-      {/* Barre jaune gauche */}    
-<Image
-  src="/assets/card-left-bar.svg"
-  alt=""
-  width={3}
-  height={200}
-  className="absolute top-[1px] left-[1px] z-[0] select-none"
-/>
+      {/* Barre jaune gauche */}
+      <Image
+        src="/assets/card-left-bar.svg"
+        alt=""
+        width={3}
+        height={200}
+        className="absolute top-[1px] left-[1px] z-[0] select-none"
+      />
 
       {/* Bouton fermer */}
       <button className="cursor-pointer absolute top-[7px] right-[9.5px] py-0.5 px-1 bg-transparent">
@@ -21,7 +21,7 @@ const Container: NextPage = () => {
         </b>
       </button>
 
-      {/* Icône + textes SVG */}
+      {/* Icône + textes */}
       <div className="self-stretch flex items-center py-0 pl-2 pr-0.5 gap-2.5 z-[2]">
         <Image
           width={34}
@@ -32,15 +32,12 @@ const Container: NextPage = () => {
         />
 
         <div className="flex flex-col items-start">
-          <div className="flex flex-col items-start">
-            <b className="relative tracking-[2.46px] leading-[14px] uppercase">
-              Application
-            </b>
-          </div>
-          <div className="self-stretch flex flex-col items-start text-[rgba(255,255,255,0.4)]">
-            <div className="relative tracking-[2.46px] leading-[14px] uppercase font-semibold">
-              Windows
-            </div>
+          <b className="tracking-[2.46px] leading-[14px] uppercase">
+            Application
+          </b>
+
+          <div className="text-[rgba(255,255,255,0.4)] tracking-[2.46px] leading-[14px] uppercase font-semibold">
+            Windows
           </div>
         </div>
       </div>
