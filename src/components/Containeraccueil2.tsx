5f4e9ecafd4fd711785c0705404e93d821b208c6
Onder <weispecher.onder@gmail.com>
2026-07-02 01:10:58 +0200

    update containeraccueil2 + fix svg

--- src/components/Containeraccueil2.tsx
+++ src/components/Containeraccueil2.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 const Containeraccueil2: NextPage = () => {
   return (
     <div className="w-full max-w-[2174px] mx-auto flex flex-col items-center pt-0 px-0 pb-6 box-border opacity-100 leading-[normal] tracking-[normal]">
+
+      {/* Flèche */}
       <Image
         className="w-full h-[18px] relative"
         width={18}
@@ -12,6 +14,10 @@ const Containeraccueil2: NextPage = () => {
         alt=""
         src="/assets/iconfleche2.svg"
       />
+
+      {/* Trait jaune */}
+      <div className="w-full h-[2px] bg-[linear-gradient(90deg,rgba(0,0,0,0)_0%,rgba(232,160,32,0.27)_50%,rgba(0,0,0,0)_100%)]"></div>
+
     </div>
   );
 };
