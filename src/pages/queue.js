a393bfa9fcf49b5caa136cc3edac7b9d41aa27b2
Onder <weispecher.onder@gmail.com>
2026-07-03 05:28:07 +0200

    Update frontend

--- src/pages/queue.js
+++ src/pages/queue.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 // 🖥️ Version desktop Queue
 import Appqueue from "../components/Appqueue";
 import Appqueue2 from "../components/Appqueue2";
-import Appqueue3 from "../components/Appqueue3";
+/*import Appqueue3 from "../components/Appqueue3"; */
 
 /*
 // 📱 Version mobile Queue
@@ -38,7 +38,6 @@ export default function Queue() {
       <div className="hidden md:flex flex-col items-center justify-center w-full">
         <Appqueue />
         <Appqueue2 />
-        <Appqueue3 />
       </div>
 
       {/* 📱 MOBILE */}
